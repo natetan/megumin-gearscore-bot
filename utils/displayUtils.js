adefa8c705b5b4292fd634a3807c4c881a4146cb
Nate Tan <nytan67@gmail.com>
2020-12-11 15:01:32 -0800

    fix nothing

--- utils/displayUtils.js
+++ utils/displayUtils.js
@@ -6,7 +6,12 @@ const getGs = (ap, dp, awk) => {
   return Math.round((ap + awk) / 2.00) + dp;
 }
 
+const getRandomArrayIndex = array => {
+  return Math.floor(Math.random() * array.length);
+}
+
 module.exports = {
   getRandom,
-  getGs
+  getGs,
+  getRandomArrayIndex
 }
